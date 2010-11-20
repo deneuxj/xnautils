9a53ee62f514d494c68ae0e27715a4178b219958
johdex <johdex@localhost>
2010-11-20 13:34:38 +0100

    Added class to track button presses and releases on a gamepad.

---
+++ XNAUtils/InputChanges.fs
@@ -0,0 +1,18 @@
+﻿module XNAUtils.InputChanges
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Input
+
+type InputChanges(player : PlayerIndex) =
+    let mutable oldState = GamePad.GetState(player)
+    let mutable newState = GamePad.GetState(player)
+
+    member x.Update() =
+        oldState <- newState
+        newState <- GamePad.GetState(player)
+
+    member x.IsButtonPress(b : Buttons) =
+        oldState.IsButtonDown(b) && newState.IsButtonUp(b)
+
+    member x.IsButtonRelease(b : Buttons) =
+        oldState.IsButtonUp(b) && newState.IsButtonDown(b)
