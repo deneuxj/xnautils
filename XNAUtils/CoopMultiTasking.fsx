74cdec527c1e3c438ae10eb975c6e5ad6aaaf3c1
johdex <johdex@localhost>
2011-01-06 15:28:14 +0100

    Added BlockedNextFrame to task states, to make it possible for a task to signal that it wants to wait until next frame starts. Other clean-ups and improvements.

--- XNAUtils/CoopMultiTasking.fsx
+++ XNAUtils/CoopMultiTasking.fsx
@@ -5,6 +5,7 @@ open XNAUtils.Heap
 type Eventually<'R> =
     | Completed of 'R
     | Blocked of float32 * (unit -> Eventually<'R>)
+    | BlockedNextFrame of (unit -> Eventually<'R>)
     | Running of (unit -> Eventually<'R>)
     | Yield of (unit -> Eventually<'R>)
 
@@ -13,6 +14,7 @@ let rec bind k e =
     | Completed(r) -> Running(fun () -> k r)
     | Running(work) -> Running(fun () -> bind k (work()))
     | Blocked(dt, work) -> Blocked(dt, fun () -> bind k (work()))
+    | BlockedNextFrame(work) -> BlockedNextFrame(fun () -> bind k (work()))
     | Yield(work) -> Yield(fun () -> work() |> bind k)
 
 let result r = Completed(r)
@@ -32,6 +34,7 @@ let rec catch e =
     match e with
     | Completed r -> result(Ok r)
     | Blocked(dt, work) -> Blocked(dt, fun () -> newWork work)
+    | BlockedNextFrame(work) -> BlockedNextFrame(fun () -> newWork work)
     | Running work -> Running(fun() -> newWork work)
     | Yield work -> Yield(fun() -> newWork work)
 
@@ -86,31 +89,52 @@ type TaskBuilder() =
 (* Task API *)
 let task = TaskBuilder()
 
+// Wait a fixed amount of time.
 let wait dt =
     Blocked(dt, fun () -> Completed())
 
-let next () =
+// Wait until next frame, i.e. next call to Scheduler.RunFor
+let nextFrame () =
+    BlockedNextFrame(fun() -> Completed())
+
+// Stop executing, let some other task execute, if any is ready. Otherwise, we get control back.
+let nextTask () =
     Yield(fun() -> Completed())
 
-let waitCond f = task {
-    while not (f()) do
-        do! next()
+// Wait until a specified condition is true.
+// nextTask is always called, then the condition is checked.
+// If it's false, we wait until next frame, check the condition,
+// call nextTask if it's not true, and so on...
+let waitUntil f = task {
+    let stop = ref false
+    while not !stop do
+        do! nextTask()
+        if f() then
+            stop := true
+        else
+            do! nextFrame()
+            stop := f()
 }
 
+// Lock, can be used for mutual exclusion.
+// Locks should not be shared across instances of Scheduler.
 type Lock() =
     let mutable locked = false;
 
     member this.Grab() =
         task {
-            do! waitCond (fun() -> not locked)
+            do! waitUntil (fun() -> not locked)
             locked <- true
         }
 
     member this.Release() =
         task {
             locked <- false
+            do! nextTask()
         }
 
+// A blocking channel which can be used for cross-task communication.
+// Note that sending blocks until the message is received.
 type BlockingChannel<'M>() =
     let mutable content = None
     let mutable flag_send = false
@@ -118,20 +142,20 @@ type BlockingChannel<'M>() =
 
     member this.Send(m : 'M) =
         task {
-            do! waitCond (fun () -> not flag_send)
+            do! waitUntil (fun () -> not flag_send)
             content <- Some m
             flag_send <- true
-            do! waitCond (fun () -> flag_receive)
+            do! waitUntil (fun () -> flag_receive)
             flag_send <- false
             content <- None
         }
 
-    member this.Read() =
+    member this.Receive() =
         task {
-            do! waitCond (fun () -> flag_send)
+            do! waitUntil (fun () -> flag_send)
             let ret = content.Value
             flag_receive <- true
-            do! waitCond (fun () -> not flag_send)
+            do! waitUntil (fun () -> not flag_send)
             flag_receive <- false
             return ret
         }
@@ -157,11 +181,14 @@ let step dt = function
             (f(), dt - w)
         else
             (Blocked(w-dt, f), 0.0f)
+    | BlockedNextFrame(f) as v -> (v, dt)
     | Running(_) as s->
         let mutable s = s
         while isRunning s do
-            let (Running f) = s
-            s <- f()
+            match s with
+            | Running f ->
+                s <- f()
+            | _ -> failwith "Unreachable"
         (s, dt)
     | Yield(f) ->
         (Running(f), dt)
@@ -170,8 +197,14 @@ let step dt = function
 (* Execution *)
 
 type Scheduler() =
+    // Ordered queue of tasks.
     let tasks : Heap<Eventually<unit> * int> = newHeap 4
+
+    // Next id to use for tie-breaks.
+    // It's important that tasks with identical priorities are added and picked in a FIFO fashion.
     let mutable id = System.Int32.MinValue
+
+    // Ordering of tasks in the queue. RunFor repeatedly picks the minimum task.
     let cmp ((ev1, n1), (ev2, n2)) =
         match ev1, ev2 with
         
@@ -180,14 +213,23 @@ type Scheduler() =
         | Yield _, Yield _
         | Completed _, Completed _ -> n1 <= n2
 
+        // Running is minimal
         | Running _, _ -> true
 
+        // Then comes Yield
         | Yield _, Running _ -> false
         | Yield _, _ -> true
         
-        | Blocked _, Running _ | Blocked _, Yield _ -> false
+        // BlockedNextFrame before Blocked. RunFor turns these to Blocked(time to next frame, f).
+        | BlockedNextFrame _, Running _ | BlockedNextFrame _, Yield _ -> false
+        | BlockedNextFrame _, _ -> true
+
+        // Blocked, ordered by time to wake-up.
+        | Blocked _, Running _ | Blocked _, Yield _ | Blocked _, BlockedNextFrame _ -> false
         | Blocked (t1, _), Blocked (t2, _) -> t1 < t2 || t1 = t2 && n1 <= n2
         | Blocked _, Completed _ -> true
+
+        // Completed is maximal (never executed)
         | Completed _, _ -> false
 
     let peekTask() =
@@ -210,15 +252,18 @@ type Scheduler() =
         tasks.arr.[0..tasks.count-1]
         |> Array.exists (function Completed _, _ -> false | _ -> true)
 
+    // Run tasks for the current frame. dt is the frame time (typically 0.016666... on Xbox for 60 fps)
     member x.RunFor(dt) =
+        // Let time pass when there are no task ready to be executed.
         let decrBlockingTimes delta =
             for i in 0..tasks.count-1 do
                 tasks.arr.[i] <-
                     match tasks.arr.[i] with
-                    | Yield _, _ | Running _, _ -> failwith "All tasks should be blocked."
+                    | Yield _, _ | Running _, _ | BlockedNextFrame _, _ -> failwith "All tasks should be blocked."
                     | Blocked (t, f), n -> Blocked (t-delta, f), n
                     | Completed (), _ as v -> v
 
+        // Repeatedly pick and execute a task, until there are only blocked tasks left (with a waiting time larger than the time to the next frame).
         let rec work dt =
             if dt > 0.0f && tasks.count > 0 then
                 let t = peekTask()
@@ -228,6 +273,10 @@ type Scheduler() =
                     f()
                     |> insertTask
                     work dt
+                | BlockedNextFrame f ->
+                    takeTask()
+                    Blocked(dt, f)
+                    |> insertTask
                 | Completed () ->
                     takeTask()
                     work dt
@@ -244,19 +293,12 @@ type Scheduler() =
         work dt
 
 
-let runAllCompressed evs =
-    let mutable state = evs
-    printfn "%A" state
-    while not (state |> Array.forall hasCompleted) do
-        let max_time =
-            state
-            |> Seq.map(function Blocked (t, _) -> t | _ -> 0.0f)
-            |> Seq.min
-        printfn "Start sweep (%f)" max_time
-        state <- state |> Array.mapi (fun i x -> printfn "Task %d>" i; step max_time x |> fst)
-        printfn "%A" state
-    state
-    |> Array.map (function Completed r -> r | _ -> failwith "Unreachable")
+let toEventuallyObj ev = task {
+    let! res = ev
+    return box res
+}
+
+(* tests *)
 
 let runAllFixed dt evs =
     let scheduler = new Scheduler()
@@ -267,12 +309,6 @@ let runAllFixed dt evs =
     while scheduler.HasLiveTasks do
         scheduler.RunFor(dt)
 
-let toEventuallyObj ev = task {
-    let! res = ev
-    return box res
-}
-
-(* tests *)
 let test1() =
     let t1 = task {
         for i in 0..3 do
@@ -282,24 +318,15 @@ let test1() =
     runAllFixed 1.0f [|t1|]
 
 let test2() =
-    let b = ref false
+    let c = BlockingChannel<string>()
 
     let sender = task {
-        b := false
-        do! next()
-        printfn "Sending"
-        b := true
-        printfn "Sent"
-        do! next()
-        printfn "Resetting"
-        b := false
-        printfn "Reset"
+        do! c.Send("Hello receiver")
     }
 
     let receiver = task {
-        printfn "Waiting"
-        do! waitCond (fun () -> !b)
-        printfn "Received"
+        let! msg = c.Receive()
+        printfn "Received: %s" msg
     }
 
     let sys = [| sender ; receiver |]
