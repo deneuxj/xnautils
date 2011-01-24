6beb2161d9e39af146087d016b07d11f6bbbdb9c
johdex <johdex@localhost>
2011-01-24 11:01:52 +0100

    The base screen now has a reference to the ScreenManager. This makes it easy for screens to add other screens.

--- XNAUtils/ScreenManager.fs
+++ XNAUtils/ScreenManager.fs
@@ -14,15 +14,85 @@ type Screen =
         abstract member UnloadContent : unit -> unit
         abstract member SetIsOnTop : bool -> unit
         abstract member SetGame : Game -> unit
+        abstract member SetScreenManager : ScreenManager -> unit
+        abstract member ClearScreenManager : unit -> unit
     end
+
+
+// The screen manager keeps track of screens which must be drawn.
+// It is responsible for telling screens when to load and unload content.
+and ScreenManager(game) =
+    inherit DrawableGameComponent(game)
+
+    let screens = new List<Screen>()
+    let screens_tmp = new List<Screen>()
+
+    let get_top_screen() =
+        if screens.Count > 0 then
+            let s = screens.[screens.Count - 1]
+            Some s
+        else
+            None
+
+    override this.LoadContent() =
+        for s in screens do s.LoadContent()
+        base.LoadContent()
+
+    override this.Update(gt) =
+        base.Update(gt)
+
+    override this.Draw(gt) =
+        // Copy screens, so that s.Draw can modify it while we are iterating over the copy.
+        screens_tmp.AddRange(screens)
+        for s in screens_tmp do s.Draw(gt)
+        screens_tmp.Clear()
+
+        base.Draw(gt)
+
+    member this.AddScreen(s : Screen) =
+        match get_top_screen() with
+        | Some top -> top.SetIsOnTop(false)
+        | None -> ()
+
+        screens.Add(s)
+
+        s.SetGame(game)
+        s.SetScreenManager(this)
+        s.SetIsOnTop(true)
+        
+        s.LoadContent()
+
+        System.Diagnostics.Debug.WriteLine(sprintf "Screen %s added." (s.GetType().Name))
+
+    member this.RemoveScreen(s : Screen) =
+        match get_top_screen() with
+        | None -> ()
+        | Some top ->
+            if System.Object.ReferenceEquals(s, top) then
+                s.SetIsOnTop(false)
+            screens.Remove(s)
+            |> function false -> failwith "Failed to remove screen" | true -> ()
+
+        match get_top_screen() with
+        | None -> ()
+        | Some top ->
+            top.SetIsOnTop(true)
+
+        s.UnloadContent()
+        s.ClearScreenManager()
+
+        System.Diagnostics.Debug.WriteLine(sprintf "Screen %s removed." (s.GetType().Name))
+
 
 // A screen implementation that provides its own content manager and access to the game object.
 [<AbstractClass>]
 type ScreenBase(relative_content_path) =
+
     let is_on_top = ref false
     let game : Game option ref = ref None
     let content : ContentManager option ref = ref None
-    
+    let screen_manager : ScreenManager option ref = ref None
+
     abstract member SetGame : Game -> unit
     default this.SetGame(new_game) =
         let g, c =
@@ -57,8 +127,15 @@ type ScreenBase(relative_content_path) =
             match !content with
             | Some content -> content.Unload()
             | None -> ()
-
         member this.SetIsOnTop(b) = is_on_top := b
+        member this.SetScreenManager(s) =
+            match !screen_manager with
+            | None -> screen_manager := Some s
+            | Some _ -> invalidOp "A screen cannot be added to more than one screen manager at a time."
+        member this.ClearScreenManager() =
+            match !screen_manager with
+            | Some _ -> screen_manager := None
+            | None -> invalidOp "Screen is not attached to a screen manager."
 
     interface System.IDisposable with
         member this.Dispose() =
@@ -70,78 +147,29 @@ type ScreenBase(relative_content_path) =
 
     member this.IsOnTop = !is_on_top
 
+    member this.IsActive =
+        !is_on_top
+#if XBOX360
+        && not Microsoft.Xna.Framework.GamerServices.Guide.IsVisible
+#endif
+
+    member this.HasGame = (!game).IsSome
+
     member this.Game =
         match !game with
         | Some g -> g
         | None -> invalidOp "Game not set"
+
+    member this.HasContent = (!content).IsSome
 
     member this.Content =
         match !content with
         | Some c -> c
         | None -> invalidOp "Content not set"
 
+    member this.HasScreenManager = (!screen_manager).IsSome
 
-// The screen manager keeps track of screens which must be drawn.
-// It is responsible for telling screens when to load and unload content.
-type ScreenManager(game) =
-    inherit DrawableGameComponent(game)
-
-    let screens = new List<Screen>()
-
-    let get_top_screen() =
-        if screens.Count > 0 then
-            let s = screens.[screens.Count - 1]
-            Some s
-        else
-            None
-
-    let add_screen (s : Screen) =
-        match get_top_screen() with
-        | Some top -> top.SetIsOnTop(false)
-        | None -> ()
-
-        screens.Add(s)
-
-        s.SetGame(game)
-        s.SetIsOnTop(true)
-        
-        s.LoadContent()
-
-        System.Diagnostics.Debug.WriteLine(sprintf "Screen %s added." (s.GetType().Name))
-
-    let remove_screen (s : Screen) =
-        match get_top_screen() with
-        | None -> ()
-        | Some top ->
-            if System.Object.ReferenceEquals(s, top) then
-                s.SetIsOnTop(false)
-            screens.Remove(s)
-            |> function false -> failwith "Failed to remove screen" | true -> ()
-
-        match get_top_screen() with
-        | None -> ()
-        | Some top ->
-            top.SetIsOnTop(true)
-
-        s.UnloadContent()
-
-        System.Diagnostics.Debug.WriteLine(sprintf "Screen %s removed." (s.GetType().Name))
-
-    override this.LoadContent() =
-        for s in screens do s.LoadContent()
-        base.LoadContent()
-
-    override this.Update(gt) =
-        base.Update(gt)
-
-    override this.Draw(gt) =
-        // Copy screens, so that s.Draw can modify it while we are iterating over the copy.
-        let screens = screens.ToArray()
-        for s in screens do s.Draw(gt)
-        base.Draw(gt)
-
-    member this.AddScreen(s : Screen) =
-        add_screen s
-
-    member this.RemoveScreen(s : Screen) =
-        remove_screen s
+    member this.ScreenManager =
+        match !screen_manager with
+        | Some s -> s
+        | None -> invalidOp "Screen must be added to a screen manager first"
