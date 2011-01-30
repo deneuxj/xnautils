9c2458a739c2a330d4bb48e93b7d3c1cf420e33e
johdex <johdex@localhost>
2011-01-30 19:29:58 +0100

    Added user settings menus.

--- Samples/CoopMultitaskingSample/Lib/Main.fs
+++ Samples/CoopMultitaskingSample/Lib/Main.fs
@@ -7,10 +7,12 @@ open XNAUtils.ScreenManager
 open XNAUtils.PressStartScreen
 open XNAUtils.MenuScreen
 open XNAUtils.TextScreen
+open XNAUtils.StorageTasks
 
-open CoopMultiTaskingSample.GameplayScreen
-open CoopMultiTaskingSample.ResultScreen
-open CoopMultiTaskingSample.MiscScreens
+open GameplayScreen
+open ResultScreen
+open MiscScreens
+open UserSettings
 
 type MainMenuEntries =
     | Play
@@ -21,13 +23,24 @@ type MainMenuEntries =
     | Exit
 
 
-type Main(game : Game, screen_manager : ScreenManager) =
+type Main<'G  when 'G :> Game and 'G :> IBackgroundColorHolder>(game : 'G, screen_manager : ScreenManager) =
     inherit DrawableGameComponent(game)
 
     let scheduler = new Scheduler()
     let sys = new Environment(scheduler)
+    let storage = new Storage(true)
 
-    let rec menu_loop exit_game controlling_player = task {
+    let menu_placement =
+        { left = 300.0f;
+          top = 100.0f;
+          spacing = 40.0f }
+
+    let menu_animation =
+        { period = 0.2f;
+          shift = 0.05f }
+
+    let rec menu_loop exit_game controlling_player data = task {
+
         use menu =
             new MenuScreen<_>(
                 content_path,
@@ -39,11 +52,8 @@ type Main(game : Game, screen_manager : ScreenManager) =
                    Scores, "Scores"
                    Credits, "Credits"
                    Exit, "Exit" |],
-                { period = 0.2f
-                  shift = 0.05f },
-                { left = 300.0f
-                  top = 100.0f
-                  spacing = 40.0f }
+                menu_animation,
+                menu_placement
             )
         screen_manager.AddScreen(menu)
         let! action = menu.Task
@@ -67,14 +77,21 @@ type Main(game : Game, screen_manager : ScreenManager) =
                 do! results.Task
                 screen_manager.RemoveScreen(results)
 
-            do! menu_loop exit_game controlling_player
+            do! menu_loop exit_game controlling_player data
         | Some Instructions ->
             use instructions = mkInstructions controlling_player sys
             screen_manager.AddScreen(instructions)
             do! instructions.Task
             screen_manager.RemoveScreen(instructions)
-            do! menu_loop exit_game controlling_player
-
+            do! menu_loop exit_game controlling_player data
+        | Some Options ->
+            use settings = mkUserSettingsScreen content_path controlling_player sys menu_animation menu_placement storage
+            screen_manager.AddScreen(settings)
+            let! choice = settings.Task
+            screen_manager.RemoveScreen(settings)
+            let! data' = handleUserSettingsMenu content_path controlling_player sys menu_animation menu_placement screen_manager storage data choice
+            data'.Apply(game)
+            do! menu_loop exit_game controlling_player data'
         | _ -> () // TODO. For now we send back to the press start screen
     }
 
@@ -86,7 +103,23 @@ type Main(game : Game, screen_manager : ScreenManager) =
             let! controlling_player = press_start.Task
             screen_manager.RemoveScreen(press_start)
 
-            do! menu_loop exit_game controlling_player
+            if storage.TitleStorage.IsNone then
+                do! storage.InitTitleStorage
+
+            storage.Player <- Some controlling_player
+            do! storage.InitPlayerStorage
+
+            let! data = task {
+                if storage.PlayerStorage.IsSome then
+                    let! maybe_data = storage.DoPlayerStorage(user_container, loadXml user_settings_filename)
+                    match maybe_data with
+                    | Some(Some d) -> return d
+                    | _ -> return (new Data())
+                else
+                    return (new Data())
+            }
+            data.Apply(game)
+            do! menu_loop exit_game controlling_player data
     }
 
     override this.Initialize() =
---
+++ Samples/CoopMultitaskingSample/Lib/UserSettings.fs
@@ -0,0 +1,102 @@
+﻿module CoopMultiTaskingSample.UserSettings
+
+open Microsoft.Xna.Framework
+
+open XNAUtils.CoopMultiTasking
+open XNAUtils.ScreenManager
+open XNAUtils.MenuScreen
+open XNAUtils.StorageTasks
+
+
+type IBackgroundColorHolder =
+    interface
+        abstract member SetBackgroundColor : Color -> unit
+    end
+
+
+type Data() =
+    let background_color = ref Color.CornflowerBlue
+
+    member this.BackgroundColor
+        with get() = !background_color
+        and set(c) = background_color := c
+
+    member this.Apply(bg_holder : IBackgroundColorHolder) =
+        bg_holder.SetBackgroundColor(!background_color)
+
+
+type MenuEntries =
+    | SetBackgroundColor
+    | Load
+    | Save
+
+let menu_items =
+    [| (SetBackgroundColor, "Background");
+       (Load, "Load preferences");
+       (Save, "Save preferences") |]
+
+let mkUserSettingsScreen content_path player sys anim placement (storage : Storage) =
+    let menu_items =
+        [|
+            yield (SetBackgroundColor, "Background")
+            if storage.PlayerStorage.IsSome then
+                yield (Load, "Load preferences")
+                yield (Save, "Save preferences")
+        |]
+
+    new MenuScreen<MenuEntries>(content_path, player, sys, menu_items, anim, placement)
+
+type Colors =
+    | AliceBlue
+    | CornflowerBlue
+    | BlueViolet
+    | CadetBlue
+    | Reset
+
+let handleUserSettingsMenu content_path player sys anim placement (sm : ScreenManager) (storage : Storage) (data : Data) (choice : MenuEntries option)= task {
+    match choice with
+    | Some SetBackgroundColor ->
+        let menu_items =
+            [| (AliceBlue, "Alice Blue");
+               (CornflowerBlue, "Cornflower Blue");
+               (BlueViolet, "BlueViolet");
+               (CadetBlue, "CadetBlue");
+               (Reset, "Reset to default") |]
+        use color_menu = new MenuScreen<Colors>(content_path, player, sys, menu_items, anim, placement)
+        sm.AddScreen(color_menu)
+        let! choice = color_menu.Task
+        match choice with
+        | Some AliceBlue -> data.BackgroundColor <- Color.AliceBlue
+        | Some CornflowerBlue | Some Reset -> data.BackgroundColor <- Color.CornflowerBlue
+        | Some BlueViolet -> data.BackgroundColor <- Color.BlueViolet
+        | Some CadetBlue -> data.BackgroundColor <- Color.CadetBlue
+        | None -> ()
+        sm.RemoveScreen(color_menu)
+        return data
+    | Some Load ->
+        do! storage.CheckPlayerStorage
+        match storage.PlayerStorage with
+        | Some _ ->
+            let! maybe_data = storage.DoPlayerStorage(MiscScreens.user_container, loadXml MiscScreens.user_settings_filename)
+            match maybe_data with
+            | Some(Some data') -> return data'
+            | None | Some None ->
+                do! doOnGuide <| fun() -> error "Failed to load user settings."
+                return data
+        | None ->
+            return data
+    | Some Save ->
+        do! storage.CheckPlayerStorage
+        match storage.PlayerStorage with
+        | Some _ ->
+            let! maybe_data = storage.DoPlayerStorage(MiscScreens.user_container, saveXml MiscScreens.user_settings_filename data)
+            match maybe_data with
+            | Some(Some _) -> return data
+            | None | Some None ->
+                do! doOnGuide <| fun() -> error "Failed to save user settings."
+                return data
+        | None ->
+            return data
+    | None ->
+        return data
+}
