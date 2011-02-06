e8530051e182e654e31753d6626fbf131d48e047
johdex <johdex@localhost>
2011-02-06 14:28:25 +0100

    Added extensions to GamerServices.SignedInGamerCollection that use options instead of null for non signed-in player indices.

---
+++ XNAUtils/XNAExtensions.fs
@@ -0,0 +1,14 @@
+﻿module XNAUtils.XNAExtensions
+
+open Microsoft.Xna.Framework
+
+type GamerServices.SignedInGamerCollection with
+    member this.ItemOpt(i : int) =
+        match this.[i] with
+        | null -> None
+        | s -> Some s
+
+    member this.ItemOpt(player : PlayerIndex) =
+        match this.[player] with
+        | null -> None
+        | s -> Some s
