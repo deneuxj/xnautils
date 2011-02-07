627cec7b2769ae5913e514dab28623c9eb4984a4
johdex <johdex@localhost>
2011-02-07 14:42:59 +0100

    Score screen exited when pressing A, Start, B or Back.

--- XNAUtils/InputChanges.fs
+++ XNAUtils/InputChanges.fs
@@ -19,12 +19,22 @@ type InputChanges(player : PlayerIndex) =
 
     // Some typical button presses
 
-    // Start or A: To start a sequence
+    // Start or A: To start a sequence, confirm, go ahead.
     member x.IsStartPressed() =
         x.IsButtonPress(Buttons.A) || x.IsButtonPress(Buttons.Start)
 
-    // B or Back: To cancel a sequence
-    // Also used to pause before continuing.
-    // Using A is dangerous as it's typically used in the sequence that just ended.
+    // B or Back: To cancel a sequence, go back to the main menu...
     member x.IsBackPressed() =
         x.IsButtonPress(Buttons.B) || x.IsButtonPress(Buttons.Back)
+
+    member x.IsMenuLeft() =
+        x.IsButtonPress(Buttons.DPadLeft) || x.IsButtonPress(Buttons.LeftThumbstickLeft)
+
+    member x.IsMenuRight() =
+        x.IsButtonPress(Buttons.DPadRight) || x.IsButtonPress(Buttons.LeftThumbstickRight)
+
+    member x.IsMenuUp() =
+        x.IsButtonPress(Buttons.DPadUp) || x.IsButtonPress(Buttons.LeftThumbstickUp)
+
+    member x.IsMenuDown() =
+        x.IsButtonPress(Buttons.DPadDown) || x.IsButtonPress(Buttons.LeftThumbstickDown)
