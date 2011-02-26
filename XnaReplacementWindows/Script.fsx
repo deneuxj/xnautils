d7d6372e1ca3511deb4725853cc0274f23bb7387
johdex <johdex@localhost>
2011-02-26 22:06:21 +0100

    Script for testing.

--- XnaReplacementWindows/Script.fsx
+++ XnaReplacementWindows/Script.fsx
@@ -2,6 +2,22 @@
 // It can be used to explore and test the library project.
 // Note that script files will not be part of the project build.
 
-#load "Module1.fs"
-open Module1
+#r "Microsoft.Xna.Framework.dll"
 
+#load "Dialogs.fs"
+#load "Storage.fs"
+
+open Microsoft.Xna.Framework.Storage
+
+async {
+    let res = StorageDevice.BeginShowSelector(null, null)
+    let! _ = Async.AwaitIAsyncResult(res)
+    let dev = StorageDevice.EndShowSelector(res)
+
+    if dev <> null then
+        let res = dev.BeginOpenContainer("Test", null, null)
+        let! _ = Async.AwaitIAsyncResult(res)
+        let container = dev.EndOpenContainer(res)
+        ()
+}
+|> Async.StartImmediate
