74128649a301d3f00f705928033419eb0778e15b
johdex <johdex@localhost>
2011-04-25 19:07:34 +0200

    Added a convenient function to group sequences of pairs by first component. The associated values are the second components.

---
+++ XNAUtils/SeqUtil.fs
@@ -0,0 +1,6 @@
+﻿module XNAUtils.SeqUtil
+
+let groupPairs (kvs : ('K * 'V) seq) : ('K * 'V seq) seq when 'K : equality =
+    kvs
+    |> Seq.groupBy fst
+    |> Seq.map (fun (k, v) -> (k, v |> Seq.map snd))
