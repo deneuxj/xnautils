14aed1e65d9687c0f6d44dc8a6c9f4d999dea0ad
johdex <johdex@localhost>
2011-09-20 20:56:37 +0200

    Decomposed update into lightweight update (done first), and computation-heavy update (done in parallel with draw). Added support for the initilization function that loads assets.

--- XNAUtils/ParallelUpdateDrawGameComponent.fs
+++ XNAUtils/ParallelUpdateDrawGameComponent.fs
@@ -3,11 +3,13 @@
 open Microsoft.Xna.Framework
 open System.Threading
 
-/// An updateable game component which performs a draw on a separate thread in parallel of update.
-/// update_fun takes a game time and an immutable state and returns a state.
+/// An updateable and drawable game component which performs a light updates on the main thread,
+/// then draw on a separate thread in parallel of more computation-heavy updates.
+/// initialize_fun is called when assets are loaded.
+/// update_fun and compute_fun take a game time and an immutable state and returns a state.
 /// draw_fun takes a game time and an immutable state and returns nothing.
-type ParallelUpdateDrawGameComponent<'State>(game, initial_state : 'State, update_fun, draw_fun) =
-    let impl = new GameComponent(game)
+type ParallelUpdateDrawGameComponent<'State>(game, initial_state : 'State, initialize_fun, update_fun, compute_fun, draw_fun) =
+    let impl = new DrawableGameComponent(game)
 
     let mutable state = initial_state
     let mutable gt_shared = GameTime()
@@ -19,20 +21,20 @@ type ParallelUpdateDrawGameComponent<'State>(game, initial_state : 'State, updat
     let mutable kill_requested = false
     let signal_done = new AutoResetEvent(false)
 
-    let do_update() =
+    let do_compute() =
         while not kill_requested do
             signal_start.WaitOne() |> ignore
-            state <- update_fun gt_shared state
+            state <- compute_fun gt_shared state
             signal_done.Set() |> ignore
 
-    let update_thread = new Thread(new ThreadStart(do_update))
+    let update_thread = new Thread(new ThreadStart(do_compute))
 #if XBOX360
     // Set affinity
     do update_thread.SetProcessorAffinity([|3|]) // 0 and 2 are reserved, I assume the "main" thread is 1.
 #endif
 
     // Must be called from the main thread.
-    let post_update_then_draw gt =
+    let post_compute_then_draw gt =
         if not kill_requested then
             let state = state
             gt_shared <- gt
@@ -41,7 +43,9 @@ type ParallelUpdateDrawGameComponent<'State>(game, initial_state : 'State, updat
             signal_done.WaitOne() |> ignore
 
     interface IGameComponent with
-        member x.Initialize() = impl.Initialize()
+        member x.Initialize() =
+            impl.Initialize()
+            initialize_fun()
 
     interface IUpdateable with
         member x.Enabled = impl.Enabled
@@ -55,7 +59,23 @@ type ParallelUpdateDrawGameComponent<'State>(game, initial_state : 'State, updat
         member x.Update(gt) =
             if impl.Enabled then
                 impl.Update(gt)
-                post_update_then_draw gt
+                state <- update_fun gt state
+
+    interface IDrawable with
+        member x.Draw(gt) =
+            if impl.Visible then
+                impl.Draw(gt)
+                post_compute_then_draw gt
+            else
+                state <- compute_fun gt state
+
+        member x.Visible = impl.Visible
+        member x.add_VisibleChanged(evh) = impl.VisibleChanged.AddHandler(evh)
+        member x.remove_VisibleChanged(evh) = impl.VisibleChanged.RemoveHandler(evh)
+
+        member x.DrawOrder = impl.DrawOrder
+        member x.add_DrawOrderChanged(evh) = impl.DrawOrderChanged.AddHandler(evh)
+        member x.remove_DrawOrderChanged(evh) = impl.DrawOrderChanged.RemoveHandler(evh)
 
     member x.Dispose() =
         if update_thread.IsAlive then invalidOp "Cannot call Dispose until the draw function has been killed."
