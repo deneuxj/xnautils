0f6b67f2e379b2c97a1f0df9ee4cd5ff9a3a1c0d
johdex <johdex@localhost>
2011-10-31 11:55:44 +0100

    Bumped some version strings.

--- XNAUtils/AssemblyInfo.fs
+++ XNAUtils/AssemblyInfo.fs
@@ -28,5 +28,5 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 [<assembly: ComVisible(false)>]
-[<assembly: AssemblyVersion("0.1.0.*")>]
+[<assembly: AssemblyVersion("0.2.0.*")>]
 do ignore()
