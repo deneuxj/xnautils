3acb4871bd9ccb5821e94374ccac3e3b2a980248
johdex <johdex@localhost>
2011-10-31 11:13:10 +0100

    Added AssemblyInfo files.

---
+++ Core/AssemblyInfo.fs
@@ -0,0 +1,39 @@
+﻿namespace CleverRake.XnaUtils.Core
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("CleverRake.XnaUtils.Core")>]
+[<assembly: AssemblyDescription("Types and functions used by XnaUtils which do not depend on Xna.")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Core")>]
+[<assembly: AssemblyCopyright("Copyright © Johann Deneux 2011")>]
+[<assembly: AssemblyTrademark("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("20f92769-0ff0-45d2-8284-724de276cfb2")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[<assembly: AssemblyVersion("0.1.*")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+
+()
