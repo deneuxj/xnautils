9ec13b87f8bac47cd3e26ed6343dec3207bf2dec
johdex <johdex@localhost>
2011-11-04 16:11:46 +0100

    Added module Error. Allows to differentiate between exceptions cause by invalid inputs and internal errors.

---
+++ Core/Error.fs
@@ -0,0 +1,10 @@
+﻿module CleverRake.XnaUtils.Error
+
+exception InternalError of string
+
+let failInternalWith msg = raise <| InternalError(msg)
+
+exception UserError of obj option * string
+
+let failUserErrorWithData data msg = raise <| UserError(Some data, msg)
+let failUserErrorWith msg = raise <| UserError(None, msg)
