dc1210ee1df98521ff76df1ea36f3ba5a8ce44bc
Johann Deneux <johann.deneux@gmail.com>
2011-12-22 17:00:46 +0100

    Figured out how to have generic vector scaling operations. Removed specific Speed3 and Accel3 modules. Added general TypedVector module, TypedVector3's operators now use this.

--- XNAUtils/Vector3.fs
+++ XNAUtils/Vector3.fs
@@ -7,18 +7,33 @@ open CleverRake.XnaUtils.Units
 type TypedVector3<[<Measure>] 'M> =
     struct
         val v : Vector3
-        new(x, y, z) = { v = Vector3(x, y, z) }
+        new(x : float32<'M>, y : float32<'M>, z : float32<'M>) = { v = Vector3(float32 x, float32 y, float32 z) }
         new(V) = { v = V }
-
-        static member public (+) (U : TypedVector3<'M>, V : TypedVector3<'M>) = new TypedVector3<'M>(U.v + V.v)
-        static member public (-) (U : TypedVector3<'M>, V : TypedVector3<'M>) = new TypedVector3<'M>(U.v - V.v)
-        static member public (*) (k : float32, U : TypedVector3<'M>) = new TypedVector3<'M>(k * U.v)
     end
 
-module Speed3 =
-    let (*) (k : float32<s>, V : TypedVector3<m/s>) : TypedVector3<m> =
-        new TypedVector3<m>((float32 k) * V.v)
+module TypedVector =
+    let add3 (U : TypedVector3<'M>, V : TypedVector3<'M>) = new TypedVector3<'M>(U.v + V.v)
 
-module Accel3 =
-    let (*) (k : float32<s>, V : TypedVector3<m/s^2>) : TypedVector3<m/s> =
-        new TypedVector3<m/s>((float32 k) * V.v)
+    let sub3 (U : TypedVector3<'M>, V : TypedVector3<'M>) = new TypedVector3<'M>(U.v - V.v)
+
+    let dot3 (U : TypedVector3<'M>, V : TypedVector3<'N>) =
+        Vector3.Dot(U.v, V.v)
+        |> LanguagePrimitives.Float32WithMeasure<'M 'N>
+
+    let len3 (U : TypedVector3<'M>) =
+        LanguagePrimitives.Float32WithMeasure<'M> (U.v.Length())
+
+    let scale3 (k : float32<'K>, U : TypedVector3<'M>) : TypedVector3<'K 'M> =
+        let conv = LanguagePrimitives.Float32WithMeasure<'K 'M>
+        let v = Vector3.Multiply(U.v, float32 k)
+        new TypedVector3<'M 'N>(conv v.X, conv v.Y, conv v.Z)
+
+    let normalize3 (U : TypedVector3<'M>) =
+        let len = len3 U
+        scale3 ((1.0f / len), U)
+
+type TypedVector3<[<Measure>] 'M>
+with
+    static member public (*) (k, U) = TypedVector.scale3 (k, U)
+    static member public (+) (U, V) = TypedVector.add3 (U, V)
+    static member public (-) (U, V) = TypedVector.sub3 (U, V)
