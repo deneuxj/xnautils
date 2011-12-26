00e36738f8777a255649e20e07d3c7597f12c4d1
Johann Deneux <johann.deneux@gmail.com>
2011-12-26 20:23:15 +0100

    Added extension to Quaternion to create quaternions from typed vectors and angles.

---
+++ XNAUtils/QuaternionExtensions.fs
@@ -0,0 +1,9 @@
+﻿module CleverRake.XnaUtils.QuaternionExtensions
+
+open CleverRake.XnaUtils.Units
+
+type Quaternion = Microsoft.Xna.Framework.Quaternion
+
+type Microsoft.Xna.Framework.Quaternion with
+    static member public CreateFromAxisAngle(v: TypedVector3<1>, angle : float32<rad>) =
+        Quaternion.CreateFromAxisAngle(v.v, float32 angle)
