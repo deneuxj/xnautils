ae17d5ce3c14d8a92d352c9bb8a963d73617fbee
johdex <johdex@localhost>
2012-01-28 17:21:20 +0100

    Extension methods for System.Random.

---
+++ XNAUtils/Random.fs
@@ -0,0 +1,15 @@
+﻿namespace CleverRake.XnaUtils
+
+open Microsoft.Xna.Framework
+
+module Random =
+    type System.Random with
+        member this.NextSingle() = this.NextDouble() |> float32
+
+        member this.NextQuaternion() =
+            let twopi = MathHelper.TwoPi
+            Quaternion.CreateFromYawPitchRoll(twopi * this.NextSingle(), twopi * this.NextSingle(), twopi * this.NextSingle())
+        
+        member this.NextVector3(mag : float32) =
+            let q = this.NextQuaternion()
+            Vector3.Transform(Vector3.UnitX, q) * mag
