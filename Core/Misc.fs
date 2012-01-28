018d6820426f51a3c7e1f418d7ea29dbe5db4a8f
johdex <johdex@localhost>
2012-01-28 15:33:15 +0100

    Added support for "for" loops to the maybe custom workflow.

--- Core/Misc.fs
+++ Core/Misc.fs
@@ -8,21 +8,65 @@ module Maybe =
     type Maybe<'a> = option<'a>
  
     let succeed x = Some(x)
+
     let fail = None
-    let bind p rest =
+
+    let bind rest p =
         match p with
             | None -> fail
             | Some r -> rest r
-    let delay f = f()
- 
+
+    let rec whileLoop cond body =
+        if cond() then
+            match body() with
+            | Some() ->
+                whileLoop cond body
+            | None ->
+                fail
+        else
+            succeed()
+
+    let combine e1 e2 =
+        e1 |> bind (fun () -> e2)
+
+    let forLoop (xs : 'T seq) f =
+        using (xs.GetEnumerator()) (fun it ->
+                whileLoop
+                    (fun () -> it.MoveNext())
+                    (fun () -> it.Current |> f)
+            )
+
     type MaybeBuilder() =
         member b.Return(x)  = succeed x
-        member b.Bind(p, rest) = bind p rest
-        member b.Delay(f)   = delay f
+        member x.ReturnFrom(r) = r
+        member b.Bind(p, rest) = p |> bind rest
         member b.Let(p,rest) : Maybe<'a> = rest p
-   
+        member x.Combine(e1, e2) = combine e1 e2
+        member x.For(xs, body) = forLoop xs body
+        member x.Zero() = succeed()
+
     let maybe = MaybeBuilder()
 
+module MaybeTests =
+    open Maybe
+
+    let testCombine() =
+        maybe {
+            printfn "Hello"
+            return Some()
+        }
+
+    let testZero() =
+        maybe {
+            printfn "Hello"
+        }
+
+    let testFor() =
+        maybe {
+            for x in [Some "Should be printed"; None; Some "Should NOT be printed"] do
+                let! x = x
+                printfn "%s" x
+        }
 
 module RepeatUntil =
     /// Repeat f until it returns Some(x), and return x.
