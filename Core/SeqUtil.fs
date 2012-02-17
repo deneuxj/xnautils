9898ae2eee32826a3bddf5a22fb069b41f44d0c4
johdex <johdex@localhost>
2012-02-17 19:05:01 +0100

    Added the rather embarassing listZip4.

--- Core/SeqUtil.fs
+++ Core/SeqUtil.fs
@@ -12,3 +12,12 @@ let rec seqInfinite f v0 =
         yield v1
         yield! seqInfinite f v1
     }
+
+let listZip4 l1 l2 l3 l4 =
+    let rec work l1 l2 l3 l4 =
+        match l1, l2, l3, l4 with
+        | i1 :: r1, i2 :: r2, i3 :: r3, i4 :: r4 ->
+            (i1, i2, i3, i4) :: work r1 r2 r3 r4
+        | [], [], [], [] -> []
+        | _ -> failwith "Lengths don't match"
+    work l1 l2 l3 l4
