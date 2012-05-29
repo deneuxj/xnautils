c3e3ca117c43ebc9602853c89a9cb077c8969246
johdex <johdex@localhost>
2012-05-29 18:28:02 +0200

    Added 2-dimensional vectors with units of measure. Renamed Vector3.fs to TypedVector.fs.

--- XNAUtils/TypedVector.fs
+++ XNAUtils/TypedVector.fs
@@ -2,6 +2,18 @@
 
 open Microsoft.Xna.Framework
 
+/// A two-dimensional vector with a unit of measure. Built on top of Xna's Vector2.
+type TypedVector2<[<Measure>] 'M> =
+    struct
+        val v : Vector2
+        new(x : float32<'M>, y : float32<'M>) =
+            { v = Vector2(float32 x, float32 y) }
+        new(V) = { v = V }
+
+        member this.X : float32<'M> = LanguagePrimitives.Float32WithMeasure this.v.X
+        member this.Y : float32<'M> = LanguagePrimitives.Float32WithMeasure this.v.Y
+    end
+
 /// A three-dimensional vector with a unit of measure. Built on top of Xna's Vector3.
 type TypedVector3<[<Measure>] 'M> =
     struct
@@ -51,9 +63,47 @@ module TypedVector =
         else
             None
 
+    let add2 (U : TypedVector2<'M>, V : TypedVector2<'M>) =
+        new TypedVector2<'M>(U.v + V.v)
+
+    let sub2 (U : TypedVector2<'M>, V : TypedVector2<'M>) =
+        new TypedVector2<'M>(U.v - V.v)
+
+    let dot2 (U : TypedVector2<'M>, V : TypedVector2<'N>) =
+        Vector2.Dot(U.v, V.v)
+        |> LanguagePrimitives.Float32WithMeasure<'M 'N>
+
+    let len2 (U : TypedVector2<'M>) =
+        LanguagePrimitives.Float32WithMeasure<'M> (U.v.Length())
+
+    let scale2 (k : float32<'K>, U : TypedVector2<'M>) : TypedVector2<'K 'M> =
+        let conv = LanguagePrimitives.Float32WithMeasure<'K 'M>
+        let v = Vector2.Multiply(U.v, float32 k)
+        new TypedVector2<_>(conv v.X, conv v.Y)
+
+    let normalize2 (U : TypedVector2<'M>) =
+        let len = len2 U
+        scale2 ((1.0f / len), U)
+
+    let tryNormalize2 (U : TypedVector2<'M>) =
+        let len = len2 U
+        if len > LanguagePrimitives.Float32WithMeasure<'M>(1e-3f) then
+            Some <| scale2 ((1.0f/ len), U)
+        else
+            None
+
 type TypedVector3<[<Measure>] 'M>
 with
     static member public (*) (k, U) = TypedVector.scale3 (k, U)
     static member public (+) (U, V) = TypedVector.add3 (U, V)
     static member public (-) (U, V) = TypedVector.sub3 (U, V)
     member public this.Length = this |> TypedVector.len3
+    static member public Zero = TypedVector3<'M>()
+
+type TypedVector2<[<Measure>] 'M>
+with
+    static member public (*) (k, U) = TypedVector.scale2 (k, U)
+    static member public (+) (U, V) = TypedVector.add2 (U, V)
+    static member public (-) (U, V) = TypedVector.sub2 (U, V)
+    member public this.Length = this |> TypedVector.len2
+    static member public Zero = TypedVector2<'M>()
